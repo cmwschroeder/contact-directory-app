5ad6cc09d20caf5531cd4c77f4b80d3411fb5a7f
cmwschroeder <cmwschroeder@gmail.com>
2022-05-25 10:39:24 -0700

    added output, plugins, and module to webpack config

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,4 +1,8 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
+const {InjectManifest} = require('workbox-webpack-plugin');
 
 module.exports = () => {
   return {
@@ -8,20 +12,61 @@ module.exports = () => {
       install: './src/js/install.js',
       cards: './src/js/cards.js'
     },
-
     // TODO: Add the correct output
     output: {
-      
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'dist'),
     },
-
     // TODO: Add the correct plugins
     plugins: [
-     
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Webpack Plugin',
+      }),
+      new MiniCssExtractPlugin(),
+      new InjectManifest({
+        swSrc: './src/sw.js',
+        swDest: 'service-worker.js',
+      }),
+      new WebpackPwaManifest({
+        name: 'Contact Directory',
+        short_name: 'CD',
+        description: 'Get contact info!',
+        background_color: '#7eb4e2',
+        theme_color: '#7eb4e2',
+        start_url: './',
+        publicPath: './',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
     ],
-
     // TODO: Add the correct modules
     module: {
-
+      rules: [
+        {
+          test: /\.css$/i,
+          use: [MiniCssExtractPlugin.loader, 'css-loader'],
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: 'asset/resource',
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
+          },
+        },
+      ],
     }
   };
 };
